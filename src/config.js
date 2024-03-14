d53a58ef27eb75423bb70858c0de0d8ed1b14354
penkalDev <penkaldev@gmail.com>
2024-03-14 11:50:25 +0100

    Adding files from local machine

---
+++ src/config.js
@@ -0,0 +1,8 @@
+// config.js
+
+const config = {
+  serverUrl: "http://localhost:3001",
+  //serverUrl: "https://15f32a2414dbc5fc.p50.rt3.io", // Zaktualizuj na odpowiedni adres IP i port serwera
+};
+
+export default config;
---
+++ src/services/dataFetcher.js
@@ -0,0 +1,23 @@
+// dataFetcher.js
+  
+import axios from 'axios';
+
+const fetchData = async (url) => {
+  try {
+    const response = await axios.get(url);
+
+    
+    if (response.status !== 200) {
+      throw new Error('Network response was not ok');
+    }
+
+  
+    const data = response.data;
+    return data;
+  } catch (error) {
+    console.error('Error fetching data:', error);
+    throw error;
+  }
+};
+
+export default fetchData;
