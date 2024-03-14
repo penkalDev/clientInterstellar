4ff4be186b2e08218a2305c27224a545376288d5
penkalDev <penkaldev@gmail.com>
2024-03-14 12:38:39 +0100

    API link added

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 // config.js
 
 const config = {
-  serverUrl: "http://localhost:3001",
+  serverUrl: "https://serverinterstellar.onrender.com",
   //serverUrl: "https://15f32a2414dbc5fc.p50.rt3.io", // Zaktualizuj na odpowiedni adres IP i port serwera
 };
 
