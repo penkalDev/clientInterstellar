e07fba065701fb5959d0098e20074c720d68ba33
penkalDev <penkaldev@gmail.com>
2024-03-14 12:57:46 +0100

    Changes added

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@
 
 const config = {
   serverUrl: "https://serverinterstellar.onrender.com",
-  //serverUrl: "https://15f32a2414dbc5fc.p50.rt3.io", // Zaktualizuj na odpowiedni adres IP i port serwera
+ 
 };
 
 export default config;
